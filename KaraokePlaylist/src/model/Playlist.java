230540019d6409470fb531086db2cfae7c0b39e2
Renan Ribeiro <renanribeiro2001@gmail.com>
2020-06-09 19:27:19 -0300

    Arrumando algumas coisas

--- KaraokePlaylist/src/model/Playlist.java
+++ KaraokePlaylist/src/model/Playlist.java
@@ -9,19 +9,24 @@ public class Playlist {
 	
 	private ArrayList<Musica> musica = new ArrayList<Musica>();
 	Scanner scan = new Scanner(System.in);
-	Cantor cantor = new Cantor("claudio");
+	
 	
 	public void adicionarMusica() {
+		System.out.println("Digite o seu nome");
+		String nomeCantor = scan.nextLine();
+		Cantor cantor = new Cantor(nomeCantor);
+		
 		System.out.println("Digite o nome de uma musica");
 		String nomeMusica = scan.nextLine();
 		musica.add(new Musica(nomeMusica, Validacoes.darNota(), cantor));
+		
 		System.out.println("o cantor " + cantor.getNome() + " pediu a musica: " + nomeMusica + " foi adicionada na playlist");
 	}
 
 	public void mostrarMusicasPlaylist() {
 		System.out.println("As musicas que estão na playlist são: ");
 		for (Musica x : musica) {
-			System.out.println("Musica: " + x.getNomeMusica() + " Nota: " + x.getNota());
+			System.out.println("Cantor: " + x.getCantor().getNome() + " Musica: " + x.getNomeMusica() + " Nota: " + x.getNota());
 		}
 	}
 
